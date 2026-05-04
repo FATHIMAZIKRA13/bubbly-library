7ef638cf0d6fc6f473c5ffb824416a4a6bc76b3d
FATHIMAZIKRA13 <Zikraf90@gmail.com>
2026-05-04 17:08:21 +0530

    Add gunicorn for production deployment

--- run.py
+++ run.py
@@ -3,6 +3,5 @@ from app import app, db
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        print("✅ Bubbly Library database ready!")
-    print("💖 Bubbly Library is running at http://127.0.0.1:5000")
+    print("💖 Bubbly Library is running!")
     app.run(debug=True)
