4df19251c30be77ea280eb83f89cb3ed02310b56
FATHIMAZIKRA13 <Zikraf90@gmail.com>
2026-05-04 17:16:58 +0530

    Fix run.py for Render deployment

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from app import app, db
+import os
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     print("💖 Bubbly Library is running!")
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
